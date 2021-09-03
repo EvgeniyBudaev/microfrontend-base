092743a9828bf302c5ee015a11ff5613e17cdd74
Evgeniy Budaev <budaev.e@gmail.com>
2021-09-03 09:33:14 +0300

    Generic Integration

--- packages/container/src/components/App.js
+++ packages/container/src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { mount } from "marketing/MarketingApp"
+import MarketingApp from "./MarketingApp";
 
 console.log("mount", mount);
 
 export default () => {
-  return <h1>Container</h1>
+  return (
+    <>
+      <h1>Container</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  );
 };
